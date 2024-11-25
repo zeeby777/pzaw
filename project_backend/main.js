a17a971c401fc411731c12402a69742580ef49c1
asdasd <karolkleban@gmail.com>
2024-11-25 16:07:25 +0100

    lol

--- project_backend/main.js
+++ project_backend/main.js
@@ -1,10 +1,56 @@
+const fs = require('fs')
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+let pokedex = [];
+let pokeTypes = [];
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+fs.readFile('./resources/pokedex.json', function(err, data) { 
+  if (err) throw err; 
+
+  pokedex = Object.values(JSON.parse(data)); 
+}); 
+
+fs.readFile('./resources/types.json', function(err, data) { 
+  if (err) throw err; 
+
+  pokeTypes = Object.values(JSON.parse(data)); 
+}); 
+
+
+app.get('/pokemon/:pokemonId/', (req, res) => {
+  const pokemonId = req.params.pokemonId
+  res.send(pokedex[pokemonId - 1])
+});
+
+app.get('/pokemon/query/type/', (req, res) => {
+  let pokemonTypes = req.query.pokemonTypes
+  let fetchedPokemon = [];
+  // jesli 1 typ dany, zmieniamy na arraya 1 elementowego
+  if(typeof(pokemonTypes) === 'string'){
+    pokemonTypes = [pokemonTypes]  
+  }
+  pokemonTypes.forEach(pokeType => {
+    pokedex.forEach(pokemon => {
+      if(pokemon.type.includes(pokeType)){
+        fetchedPokemon.push(pokemon)
+      }
+    } )
+  });
+  res.send(fetchedPokemon)
+})
+
+app.get('/types/:lang', (req, res) => {
+  const langSchema = ['english', 'chinese', 'japanese']
+  const lang = req.params.pokemonId
+  console.log(lang)
+  let langId = langSchema.indexOf(lang)
+  let fetchedTypes = []
+  pokeTypes.forEach(pokeType => {
+    fetchedTypes.push(Object.entries(pokeType)[langId][1])
+  })
+  res.send(fetchedTypes)
 });
 
 app.listen(8080);
